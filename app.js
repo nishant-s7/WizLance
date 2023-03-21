0e4921f3ec35c7fce244d730797f53d56405801e
Anushthan Saxena <anushthans@gmail.com>
2023-03-21 16:46:22 +0530

    backup

--- app.js
+++ app.js
@@ -16,131 +16,143 @@ relation.createTable(db);
 app.use(express.urlencoded({ extended: false }));
 
 app.use(express.static(__dirname + "/public"));
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
+
 
 let loggedIn = false;
 
-app.get("/", function (req, res) {
-  loggedIn = false;
-  res.render("pages/landing_nol.ejs");
+app.get('/', function(req, res){
+    loggedIn = false;
+    res.render('pages/landing_nol.ejs');
 });
 
-app.get("/signup", (req, res) => {
-  res.render("pages/signup");
-});
 
-app.post("/signup", async (req, res) => {
-  let firstName = req.body.firstname;
-  let lastName = req.body.lastname;
-  let eMail = req.body.email;
-  let password = req.body.password;
 
-  let exist;
+app.get("/signup", (req,res)=>
+{
+  res.render("pages/signup")
+})
 
-  function checkAccountsExists(eMail) {
-    return new Promise((resolve, rejects) => {
-      db.get(`SELECT * FROM user WHERE eMail = ?`, [eMail], (err, row) => {
-        if (err) {
-          console.log(err.message);
-          rejects(err);
-        }
-        exist = !!row;
-        resolve(exist);
+
+app.post("/signup", async(req, res) => {
+    let firstName = req.body.firstname
+    let lastName = req.body.lastname
+    let eMail = req.body.email
+    let password = req.body.password
+  
+    let exist;
+  
+    function checkAccountsExists(eMail)
+    {
+     
+      return new Promise((resolve , rejects)=>{
+        db.get(`SELECT * FROM user WHERE eMail = ?` , [eMail], (err ,row)=>{
+          if(err)
+          {
+            console.log(err.message);
+            rejects(err);
+          }
+          exist = !!row;
+          resolve(exist);
+        });
       });
-    });
-  }
+      
+    }
 
-  exist = await checkAccountsExists(eMail);
-  if (exist) {
-    res.status(400).send({
-      message: "Account already exists.",
-    });
-  } else if (!exist) {
-    db.run(
-      "insert into user (fname, lname, email, password) values " +
-        "('" +
-        firstName +
-        "','" +
-        lastName +
-        "','" +
-        eMail +
-        "','" +
-        password +
-        "')",
-      (err) => {
-        if (err) {
-          console.log(err);
-        } else {
-          loggedIn = true;
-          res.redirect("mainpage");
-        }
-      }
-    );
-  }
-});
+    exist = await checkAccountsExists(eMail);
+    if (exist) {
+        res.status(400).send({
+            message: "Account already exists."
+        });
+    } else if (!exist) {
+        db.run("insert into user (fname, lname, email, password) values " + "('" + firstName + "','" + lastName + "','" + eMail + "','" + password + "')", err => {
+            if (err) {
+                console.log(err);
+            } else {
+              loggedIn = true;
+                res.redirect("mainpage");
+            }
+        });
+    }
+  
+  })
+  
+  
+  app.get("/login",(req,res)=>
+{
+  res.render("pages/login")
+})
 
-app.get("/login", (req, res) => {
-  res.render("pages/login");
-});
 
 app.post("/login", async (req, res) => {
-  let email = req.body.email_address;
-  let password = req.body.login_password;
-  await db.get(
-    "SELECT password FROM user WHERE email = ?",
-    [email],
-    (err, row) => {
-      if (err) {
-        console.error(err.message);
-        return;
-      }
-      if (row) {
-        let password_check;
-        password_check = row.password;
-        console.log(password);
-        console.log(row.password);
-        if (row.password === password) {
-          loggedIn = true;
-          res.redirect("mainpage");
-        } else {
-          res.send("Invalid login details 1");
+    let email = req.body.email_address;
+    let password = req.body.login_password;
+    await db.get("SELECT password FROM user WHERE email = ?", [email], (err, row) => {
+        if (err) {
+            console.error(err.message);
+            return;
         }
-      } else {
-        res.send("Invalid login details");
-      }
+        if (row) {
+            let password_check;
+            password_check = row.password
+            console.log(password);
+            console.log(row.password);
+            if (row.password === password) {
+                loggedIn = true;
+                res.redirect("mainpage")
+            }else {
+                res.send("Invalid login details 1");
+            }
+        } else {
+            res.send("Invalid login details");
+        }
+    });
+  })
+
+app.get("/mainpage" , (req,res)=>{
+    if(loggedIn){
+      res.render("pages/mainpage")
     }
-  );
+    else{
+      res.redirect("login")
+    }
+  });
+
+app.get('/graphics-design', (req, res) => {
+  res.render('pages/graphics-design', {loggedIn});
 });
 
-app.get("/mainpage", (req, res) => {
-  if (loggedIn) {
-    res.render("pages/mainpage");
-  } else {
-    res.redirect("login");
-  }
+app.get('/music-audio', (req, res) => {
+  res.render('pages/music-audio', {loggedIn});
 });
 
-app.get("/graphics-design", (req, res) => {
-  if (loggedIn) res.render("pages/graphics-design");
-  else res.redirect("login");
+app.get('/programming-tech', (req, res) => {
+  res.render('pages/programming-tech', {loggedIn});
 });
 
-app.get("/logo-design", (req, res) => {
-  console.log(loggedIn);
-  res.render("pages/logo-design", {loggedIn})
+app.get('/photography', (req, res) => {
+  res.render('pages/photography', {loggedIn});
 });
 
-app.get("/profile-templates", (req, res) => {
-  // if (loggedIn) res.render("pages/profile-templates");
-  // else res.redirect("login");
-
-  res.render("pages/profile-templates",{loggedIn} )
+app.get('/animation', (req, res) => {
+  res.render('pages/animation', {loggedIn});
 });
 
-app.get("/dashboard", (req, res, next) => {
-  res.render("pages/dashboard");
+app.get('/writing-translation', (req, res) => {
+  res.render('pages/writing-translation', {loggedIn});
 });
 
-app.listen(3000, function () {
-  console.log("Server listening at 3000");
+app.get('/logo-design', (req, res) => {
+  res.render('pages/logo-design', {loggedIn});
 });
+
+
+app.get('/logo-design/profile-templates', (req, res) => {
+  if(loggedIn) res.render('pages/profile-templates');
+  else res.redirect('login');
+});
+
+
+app.listen(3000, function(){
+    console.log("Server listening at 3000");
+})
