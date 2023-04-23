6b208139bc500c2b16e8f036c3d58671d9a1b285
Anushthan Saxena <anushthans@gmail.com>
2023-04-24 01:02:33 +0530

    backup

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,5 +1,6 @@
 const User = require("../models/user");
 const Category = require("../models/category");
+const Gig = require("../models/gig");
 
 exports.displayUsers = (req, res) => {
     User.find({}, {password: 0}).then(
@@ -15,4 +16,67 @@ exports.displayCategories = (req, res) => {
             return res.render('pages/display-categories', {categories})
         }
     );
+};
+
+exports.displayGigs = (req, res) => {
+    Gig.find({}).then(
+        (gigs) => {
+            return res.render('pages/display-gigs', {gigs})
+        }
+    );
+};
+exports.deleteFromUser = (req, res) => {
+    const requiredMail = req.body.deleteFromEmail;
+    User.deleteOne({email: requiredMail}).then(
+        () => {
+            return res.redirect('/admin-users');
+        }
+    );
+};
+exports.addCategory = (req, res) => {
+    const newCategory = req.body.addCategory;
+    const image = req.body.addCategoryImage;
+    const catObj = new Category({
+        name: newCategory,
+        imageUrl: image
+    });
+    catObj.save();
+    
+    setTimeout( () => { return res.redirect('/admin-categories');}, 50);
+};
+
+exports.addSubCategory = (req, res) => {
+    const requiredCategory = req.body.selectCategory;
+    const image = req.body.addSubCategoryImage;
+
+    const newSubCategory = {
+        name: req.body.addSubCategory,
+        imageUrl: image
+    }
+
+    Category.findOneAndUpdate(
+        { name: requiredCategory.toString()},{ $push: { subCategories: newSubCategory } }
+      )
+        .then(() => {
+          return res.redirect('/admin-categories');
+        })
+};
+
+exports.deleteCategory = (req, res) => {
+    const categoryChosen = req.body.selectedCategory;
+
+    Category.deleteOne({name: categoryChosen}).then(() => {
+        return res.redirect('/admin-categories');
+    });
+
+};
+
+exports.deleteSubCategory = (req, res) => {
+    const categoryChosen = req.body.selectedCategory;
+    const subCategoryChosen = req.body.deleteSubCategory;
+
+    Category.findOneAndUpdate(
+        {name: categoryChosen.toString()}, {$pull: {subCategories: {name:subCategoryChosen}}}).then(()=>{
+            return res.redirect('/admin-categories')
+        });
 };
--- routes/admin.js
+++ routes/admin.js
@@ -5,4 +5,11 @@ const router = express.Router();
 
 router.get("/admin-users", adminController.displayUsers);
 router.get("/admin-categories", adminController.displayCategories);
+router.get("/admin-gigs", adminController.displayGigs);
+
+router.post("/admin-delete", adminController.deleteFromUser);
+router.post("/admin-add-category", adminController.addCategory);
+router.post("/admin-add-subcategory", adminController.addSubCategory);
+router.post("/admin-delete-category", adminController.deleteCategory);
+router.post("/admin-delete-subcategory", adminController.deleteSubCategory);
 module.exports = router;
