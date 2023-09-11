f1fa97bca19f6631e0e9ea28527bad615b80447b
nishant-s7 <97207366+nishant-s7@users.noreply.github.com>
2023-09-11 19:33:30 +0530

    Search users ajax

--- controllers/ajax.js
+++ controllers/ajax.js
@@ -1,4 +1,5 @@
 const Category = require("../models/category");
+const User = require("../models/user");
 
 exports.searchCategories = async (req, res, next) => {
   const query = req.query.query;
@@ -7,5 +8,15 @@ exports.searchCategories = async (req, res, next) => {
   });
   res.status(200).json({
     categories,
+  });
+};
+
+exports.searchUsers = async (req, res, next) => {
+  const query = req.query.query;
+  const users = await User.find({
+    firstName: { $regex: query, $options: "i" },
+  });
+  res.status(200).json({
+    users,
   });
 };
